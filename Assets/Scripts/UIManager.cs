b0d9a9c3aa47e0d0114cb45b816d4fe381f4c23d
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-10-20 23:29:01 -0300

    check FMOD emmiter sound

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,18 @@
 using FMODUnity;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [field: Header("Sounds"), Space(5)]
     private Dictionary<Sounds, EventReference> _eventReferencesUI;
     [field: SerializeField] private Sounds ui_highlight;
     [field: SerializeField] private Sounds ui_play;
     [field: SerializeField] private Sounds ui_cancel;
     [field: SerializeField] private Sounds ui_music;
+
+
 
     private void Awake()
     {
@@ -23,16 +27,24 @@ public class UIManager : MonoBehaviour
     {
         AudioManager.instance.PlaySoundSFX(ui_highlight);
         //AudioManager.instance.Sounds[ui_highlight].
- 
-
     }
     public void PlayButton()
     {
         AudioManager.instance.PlaySoundSFX(ui_play);
     }
+
     public void BackButton()
     {
         AudioManager.instance.PlaySoundSFX(ui_cancel);
     }
 
+    public void StartScene(string value)
+    {
+        SceneManager.LoadScene(value);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
