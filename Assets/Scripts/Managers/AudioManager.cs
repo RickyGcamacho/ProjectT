d7294b47a71d55b68a94e2c8e26bfd1af426806d
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-10-24 20:20:27 -0300

    change menu

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public float test=20;
-    public float test2=20;
     public static AudioManager instance { get; private set; }
     private void Awake()
     {
@@ -23,25 +21,7 @@ public class AudioManager : MonoBehaviour
     {
         
     }
-    public void SetAttributes(Dictionary<Sounds, EventInstance> typeSound)
-    {
-        foreach (var item in typeSound)
-        {
-            FMOD.ATTRIBUTES_3D attributes = new FMOD.ATTRIBUTES_3D();
-
-            // Define la posición correcta utilizando transform.position para x, y, z
-            attributes.position = new FMOD.VECTOR
-            {
-                x = transform.position.x,
-                y = transform.position.y,
-                z = transform.position.z
-            };
-
-            // Aplica los atributos 3D al sonido
-            item.Value.set3DAttributes(attributes);
-        }
-    }
-
+   
     public void PlayOneShot(EventReference sound, Vector2 worldPos)
     {
      
@@ -52,31 +32,8 @@ public class AudioManager : MonoBehaviour
     {     
         if (typeSound.ContainsKey(soundKey))
         {
-            EventInstance instance = RuntimeManager.CreateInstance(typeSound[soundKey]);
-            var feet3DPosition = RuntimeUtils.To3DAttributes(gameObject.transform.position);
-            instance.set3DAttributes(feet3DPosition);
-
-            // Obtener la descripción del evento
-            FMOD.Studio.EventDescription eventDescription;
-            instance.getDescription(out eventDescription);
-
-            // Obtener la descripción y el ID del parámetro 'generator_condition'
-            FMOD.Studio.PARAMETER_DESCRIPTION parameterDescription;
-            FMOD.Studio.PARAMETER_DESCRIPTION parameterDescription2;
-            eventDescription.getParameterDescriptionByName("generator_condition", out parameterDescription);
-            eventDescription.getParameterDescriptionByName("Distance", out parameterDescription2);
-            FMOD.Studio.PARAMETER_ID parameterID = parameterDescription.id;
-            FMOD.Studio.PARAMETER_ID parameterID2 = parameterDescription2.id;
-
-            // Cambiar el valor del parámetro a 'start'
-            instance.setParameterByID(parameterID, test);  // 1.0f para encender
-            instance.setParameterByID(parameterID, test2);  // 1.0f para encender
-
-
+            EventInstance instance = RuntimeManager.CreateInstance(typeSound[soundKey]);        
             instance.start();
-  
-            //typeSound[soundKey].setParameterByName("Distance", test);
-            //typeSound[soundKey].start();
         }
     }
 
