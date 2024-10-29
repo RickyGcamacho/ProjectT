017b97908898e20e48a2e18206e9d7da343457d9
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-10-29 19:11:07 -0300

    crouch and walk combined sound

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -8,14 +8,16 @@ using STOP_MODE = FMOD.Studio.STOP_MODE;
 public class PlayerView : MonoBehaviour
 {
     [field: SerializeField] private Sounds _sfxWalk;
-    [field: SerializeField] private Sounds _sfxRun;
+    [field: SerializeField] private Sounds _sfxRun; //TODO
     [field: SerializeField] private Sounds _sfxCrouch;
-    [field: SerializeField] private Sounds _sfxFlashlight;
-    [field: SerializeField] private Sounds _sfxGrab;
+    [field: SerializeField] private Sounds _sfxFlashlight; //TODO
+    [field: SerializeField] private Sounds _sfxGrab; //TODO
     private Rigidbody _rb;
     private Dictionary<Sounds, EventReference> _banksPlayer;
     private EventInstance playerWalk;
     private EventInstance playerCrouch;
+    private bool canCrouch = false;
+    private bool wasCrouching = false;
 
     private void Awake()
     {
@@ -31,30 +33,39 @@ public class PlayerView : MonoBehaviour
         playerCrouch = AudioManager.instance.GetEventInstance(_banksPlayer, _sfxCrouch);
         
     }
+
+
+
     private void Update()
     {
-
-
-
+        // Detecta si el jugador está agachado
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            Movement(playerCrouch);
+            canCrouch = true;
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
-            Movement(playerWalk);
+            canCrouch = false;
+        }
+
+        // Determina qué sonido reproducir basado en el estado de agachado
+        if (canCrouch)
+        {
+            // Reproduce el sonido de agacharse si el jugador está agachado
+            playerWalk.stop(STOP_MODE.ALLOWFADEOUT); // Detiene el sonido de caminar
+            Movement(playerCrouch);
         }
         else
         {
+            // Reproduce el sonido de caminar si el jugador no está agachado
+            playerCrouch.stop(STOP_MODE.ALLOWFADEOUT); // Detiene el sonido de agacharse
             Movement(playerWalk);
         }
-
     }
 
     public void Movement(EventInstance move)
     {
-
-        if (_rb.velocity != Vector3.zero) // Reproduce el sonido 
+        if (_rb.velocity != Vector3.zero) // Reproduce el sonido cuando el jugador se mueve
         {
             PLAYBACK_STATE playbackState;
             move.getPlaybackState(out playbackState);
@@ -65,16 +76,10 @@ public class PlayerView : MonoBehaviour
         }
         else
         {
-            move.stop(STOP_MODE.ALLOWFADEOUT);
+            move.stop(STOP_MODE.ALLOWFADEOUT); // Detiene el sonido cuando el jugador deja de moverse
         }
+    }
 
-    }
-    public void Crouch()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            AudioManager.instance.PlayOneShot(_banksPlayer, _sfxCrouch);
-        }
-    }
+
 
 }
