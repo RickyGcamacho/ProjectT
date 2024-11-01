febae83d4785fa9783937cb9f7cfa052164f5c7d
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-11-01 00:02:58 -0300

    add sound ambient gameplay

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,7 +19,7 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         soundsUI = FMODEvents.instance.References_UI;
-       _event_generator = AudioManager.instance.GetEventInstance(soundsUI, ui_music);
+        AudioManager.instance.GetEventInstance(soundsUI, ui_music).start();
 
 
     }
