2a72bf87d9871f219e70443b2e69d90eb08c295c
Ricky G. Camacho <rickygcamacho@gmail.com>
2024-11-04 21:54:17 -0300

    puntero especial

--- Assets/Collaborators/Ricky/Outline shader/OutlineSelected.cs
+++ Assets/Collaborators/Ricky/Outline shader/OutlineSelected.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,6 +6,8 @@ public class OutlineSelection : MonoBehaviour
     private Transform highlight;
     private Transform selection;
     private RaycastHit raycastHit;
+    public GameObject handUI;
+    public GameObject normalUI;
 
     void Update()
     {
@@ -16,6 +16,8 @@ public class OutlineSelection : MonoBehaviour
         {
             highlight.gameObject.GetComponent<Outline>().enabled = false;
             highlight = null;
+            normalUI.SetActive(true);
+            handUI.SetActive(false);
         }
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
@@ -26,13 +28,14 @@ public class OutlineSelection : MonoBehaviour
                 if (highlight.gameObject.GetComponent<Outline>() != null)
                 {
                     highlight.gameObject.GetComponent<Outline>().enabled = true;
+                    handUI.SetActive(true);
+                    normalUI.SetActive(false);
                 }
                 else
                 {
                     Outline outline = highlight.gameObject.AddComponent<Outline>();
                     outline.enabled = true;
-                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.magenta;
-                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 7.0f;
+
                 }
             }
             else
@@ -41,7 +44,6 @@ public class OutlineSelection : MonoBehaviour
             }
         }
 
-    
     }
 
 }
